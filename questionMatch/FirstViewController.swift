e7dccd2dca23ef0ea6181bd618afd38581764619
野中柊平 <rad_muku_love.nash.131223@ezweb.ne.jp>
2018-05-18 18:24:11 +0900

    Initial Commit

---
+++ questionMatch/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  questionMatch
+//
+//  Created by 野中柊平 on 2018/05/18.
+//  Copyright © 2018年 shuhei nonaka. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ questionMatch/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  questionMatch
+//
+//  Created by 野中柊平 on 2018/05/18.
+//  Copyright © 2018年 shuhei nonaka. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
